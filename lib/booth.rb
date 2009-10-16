749feac856a702b9f342a970593d08842f7a2143
Chris Anderson <jchris@grabb.it>
2009-10-16 10:31:10 -0700

    stitch together new file structure

--- lib/booth.rb
+++ lib/booth.rb
@@ -18,35 +18,11 @@ Booth ||= {}
 
 # TODO Help! I want code reloading during dev.
 
-# mime :json, "application/json"
-
-set :public, File.join(File.expand_path(File.dirname(__FILE__)),"..","public")
+set :public, File.join(filepath,"..","public")
 
 
-# json error
-def je code, name, message
-  j code, {"error" => name, "reason" => message}
-end
-
-def j code, json
-  status code
-  content_type "json"
-  json.to_json
-end
-
-get '/' do
-  j 200, "couchdb"=>"Welcome","version"=>"0"
-end
-
-require 'uuid'
-get "/_uuids" do
-  uuid = UUID.new
-  uuids = (1..10).collect{uuid.generate}
-  j(200, {"uuids" => uuids})
-end
-
-load 'global_req.rb'
-load 'db_req.rb'
-load 'doc_req.rb'
-load 'view_req.rb'
+load 'global.rb'
+load 'db.rb'
+load 'doc.rb'
+load 'view.rb'
 
---
+++ lib/httpd/global.rb
@@ -0,0 +1,24 @@
+require 'uuid'
+
+get '/' do
+  j 200, "couchdb"=>"Welcome","version"=>"0"
+end
+
+
+get "/_uuids" do
+  uuid = UUID.new
+  uuids = (1..10).collect{uuid.generate}
+  j(200, {"uuids" => uuids})
+end
+
+
+# json error
+def je code, name, message
+  j code, {"error" => name, "reason" => message}
+end
+
+def j code, json
+  status code
+  content_type "json"
+  json.to_json
+end
