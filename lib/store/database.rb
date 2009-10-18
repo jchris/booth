c8e6177ccaa66fc75e6404af098dc357f059efc0
Chris Anderson <jchris@grabb.it>
2009-10-18 15:04:02 -0700

    by seq tree

--- lib/store/database.rb
+++ lib/store/database.rb
@@ -1,12 +1,19 @@
 require 'tree'
 require 'doc'
 class Database
+  attr_reader :seq
   def initialize
     @by_docid = Tree.new
+    @by_seq = Tree.new
+    @seq = 0
   end
   def put doc
     doc = Doc.new(doc)
+    @seq += 1
+    doc.seq = @seq
+    @by_seq[@seq] = doc.id
     @by_docid[doc.id] = doc
+    doc.id
   end
   def get docid
     @by_docid[docid]
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -13,4 +13,19 @@ describe "Database" do
     d.id.should == "foo"
     d["bam"].should == "baz"
   end
+  it "should apply a sequence" do
+    @db.put({
+      "_id" => "foo",
+      "bam" => "baz"
+    })
+    @db.put({
+      "_id" => "bar",
+      "bam" => "dog"
+    })
+    d = @db.get("foo")
+    d.seq.should == 1
+    d2 = @db.get("bar")
+    d2.seq.should == 2
+    @db.seq.should == 2
+  end
 end
